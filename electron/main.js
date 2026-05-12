f8f18c42932911e8f1ba437d096d7bacd1602069
Marcelo <marcelohdd1@gmail.com>
2026-05-12 04:34:35 -0300

    Add Electron desktop build configuration

---
+++ electron/main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    minWidth: 900,
+    minHeight: 600,
+    title: 'FIFA World Domination',
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  })
+
+  win.loadFile(path.join(__dirname, '..', 'fifa-world-domination.html'))
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
